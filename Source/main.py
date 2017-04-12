acd3e6555468a20b11e33cca6539ae6a1bb93848
Thomas Fredriksen <tfe@datarespons.no>
2017-04-12 20:24:38 +0200

    Moved "Window_SDL2.py" to "GameWindow"-folder. Added abstract GameWindow-class "AbstractGameWindow"

--- Source/main.py
+++ Source/main.py
@@ -29,9 +29,9 @@ import sys
 from multiprocessing import Process
 
 if len(sys.argv) < 2:
-    from Window_SDL2 import Window
+    from GameWindow import SdlGameWindow as Window
 elif sys.argv[1] == "SDL2":
-    from Window_SDL2 import Window
+    from GameWindow import SdlGameWindow as Window
 elif sys.argv[1] == "pygame":
     from Window_pygame import Window
 else:
