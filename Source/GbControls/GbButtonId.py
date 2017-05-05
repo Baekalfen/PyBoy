63504954a57513fbd564992f6312ee5776cdba56
Thomas Li Fredriksen <tfe@datarespons.no>
2017-05-05 12:13:12 +0200

    Changed GbButtonId. Now inherits from Enum, instead of AutoEnum.

--- Source/GbControls/GbButtonId.py
+++ Source/GbControls/GbButtonId.py
@@ -5,46 +5,73 @@
 # GitHub: https://github.com/thomafred/PyBoy
 #
 
-from GbEnum import AutoEnum
+from enum import Enum
 
 
-class GbButtonId(AutoEnum):
+class GbButtonId(Enum):
     """GB Button ID enum"""
 
-    NONE = ()           # NULL-button
-    START = ()          # START-button
-    SELECT = ()         # SELECT-button
-    A = ()              # A-button
-    B = ()              # B-button
-    DPAD_LEFT = ()      # DPAD-left
-    DPAD_RIGHT = ()     # DPAD-right
-    DPAD_UP = ()        # DPAD-up
-    DPAD_DOWN = ()      # DPAD-down
-    EMU_DEBUG = ()      # Emulator debug toggle
-    EMU_SPEED = ()      # Emulator speed toggle
-    EMU_QUIT = ()       # Emulator quit
-    EMU_LOAD = ()       # Emulator load state
-    EMU_SAVE = ()       # Emulator save state
+    DPAD_MASK   = 0x0100
+    BUTTON_MASK = 0x0200
+    EMU_MASK    = 0x0400
+
+    NONE       = 0                   # NULL-button
+    START      = BUTTON_MASK | 0x01  # START-button
+    SELECT     = BUTTON_MASK | 0x02  # SELECT-button
+    A          = BUTTON_MASK | 0x03  # A-button
+    B          = BUTTON_MASK | 0x04  # B-button
+    DPAD_LEFT  = DPAD_MASK | 0x01    # DPAD-left
+    DPAD_RIGHT = DPAD_MASK | 0x02    # DPAD-right
+    DPAD_UP    = DPAD_MASK | 0x03    # DPAD-up
+    DPAD_DOWN  = DPAD_MASK | 0x04    # DPAD-down
+    EMU_DEBUG  = EMU_MASK | 0x01     # Emulator debug toggle
+    EMU_SPEED  = EMU_MASK | 0x02     # Emulator speed toggle
+    EMU_QUIT   = EMU_MASK | 0x03     # Emulator quit
+    EMU_LOAD   = EMU_MASK | 0x04     # Emulator load state
+    EMU_SAVE   = EMU_MASK | 0x05     # Emulator save state
+
+    def __and__(self, other):
+        """AND-operator
+
+        :param other: Second term
+        """
+
+        if isinstance(other, GbButtonId):
+            return self.value & other.value
+        elif isinstance(other, int):
+            return self.value & other
+        else:
+            raise TypeError('Did not receive a gbbuttonid or int-instance')
+
+    def __or__(self, other):
+        """OR-operator
+
+        :param other: Second term
+        """
+        if isinstance(other, GbButtonId):
+            return self.value | other.value
+        elif isinstance(other, int):
+            return self.value | other
+        else:
+            raise TypeError('Did not receive a gbbuttonid or int-instance')
 
     @staticmethod
     def isButton(button):
         """Asserth whter button is a GB-button (not DPAD)"""
 
-        if not isinstance(button, GbButtonId):
-            raise TypeError('Did not receive a GbButtonId-intance')
+        if not isinstance(button, GbButtonId) and not isinstance(button, int):
+            raise TypeError('Did not receive a GbButtonId or int-instance')
 
-        return button in (GbButtonId.START, GbButtonId.SELECT,
-                GbButtonId.A, GbButtonId.B)
+        return (button & GbButtonId.BUTTON_MASK) > 0
 
     @staticmethod
     def isDpad(button):
         """Assert whether button is a DPAD-member"""
 
-        if not isinstance(button, GbButtonId):
-            raise TypeError('Did not receive a GbButtonId-instance')
+        if not isinstance(button, GbButtonId) and not isinstance(button, int):
+            raise TypeError('Did not receive a GbButtonId or int-instance')
 
-        return button in (GbButtonId.DPAD_LEFT, GbButtonId.DPAD_RIGHT,
-                GbButtonId.DPAD_UP, GbButtonId.DPAD_DOWN)
+        return (button & GbButtonId.DPAD_MASK) > 0
 
     @staticmethod
     def isNone(button):
@@ -52,10 +79,10 @@ class GbButtonId(AutoEnum):
 
         if button is None:
             return True
-        elif not isinstance(button, GbButtonId):
-            raise TypeError('Did not receive a GbButtonId-instance')
+        if not isinstance(button, GbButtonId) and not isinstance(button, int):
+            raise TypeError('Did not receive a GbButtonId or int-instance')
 
-        return button == NONE
+        return button == GbButtonId.NONE
 
     @staticmethod
     def isEmu(button):
@@ -63,8 +90,7 @@ class GbButtonId(AutoEnum):
 
         if button is None:
             return True
-        elif not isinstance(button, GbButtonId):
-            raise TypeError('Did not receive a GbButtonId-instance')
+        if not isinstance(button, GbButtonId) and not isinstance(button, int):
+            raise TypeError('Did not receive a GbButtonId or int-instance')
 
-        return button in (GbButtonId.EMU_DEBUG, GbButtonId.EMU_SPEED,
-                GbButtonId.EMU_QUIT, GbButtonId.EMU_SAVE, GbButtonId.EMU_LOAD)
+        return (button & GbButtonId.EMU_MASK) > 0
