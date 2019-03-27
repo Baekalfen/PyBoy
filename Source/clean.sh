cc1eb326bd5c6f998c59e5a1c0142e1e185e7a1f
Mads Ynddal <mads-git@ynddal.dk>
2019-03-27 13:38:14 +0100

    Moved to Makefile for building

--- Source/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find . -name "*.pyo" -delete
-find . -name "*.pyc" -delete
-find . -name "*.so" -delete
-find . -name "*.c" -delete
-find . -name "*.h" -delete
-# find . -name "*.html" -delete
