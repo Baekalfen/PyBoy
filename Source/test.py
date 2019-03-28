abd42e096ade80c46b25b3148c45046e90b7bb14
Mads Ynddal <mads-git@ynddal.dk>
2019-03-28 11:55:37 +0100

    test.py no longer needed

--- Source/test.py
+++
@@ -1,7 +0,0 @@
-import PyBoy
-
-P = PyBoy.PyBoy("SDL2", 1, "ROMs/pokemon_gold.gbc", "ROMs/DMG_ROM.bin")
-# P = PyBoy.PyBoy("SDL2", 1, "ROMs/SuperMarioLand.gb", "ROMs/DMG_ROM.bin")
-
-for n in range(200):
-    P.tick()
