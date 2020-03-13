d1504397ed3bf948b40aabfa1b4d9d2010d55eb0
Mads Ynddal <mads-git@ynddal.dk>
2020-03-13 20:32:35 +0100

    sdf

--- examples/interface_example.py
+++ examples/interface_example.py
@@ -74,5 +74,6 @@ pprint(list(tile_map[13:20, 8:14]))
 #  [5, 10, 15, 1, 24, 30, 32]]
 
 # For unattended use, the screen buffer can be displayed using the following:
+screen_image = pyboy.get_screen_image()
 if not hide_window:
-    pyboy.get_screen_image().show()
+    screen_image.show()
