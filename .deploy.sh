e3444dc72da93c0dad3cc4f4f3c50f4c75a8a8ae
Mads Ynddal <mads-git@ynddal.dk>
2020-04-13 23:15:27 +0200

    manylinux workign

--- .deploy.sh
+++ .deploy.sh
@@ -10,15 +10,15 @@ else
     PY=python3
 fi
 
-"$PY" -m pip install wheel twine
-"$PY" setup.py sdist bdist_wheel
+$PY -m pip install wheel twine
+$PY setup.py sdist bdist_wheel
 
 if [ "$PLAT" = "manylinux2014_x86_64" ]; then
-    "$PY" -m pip install auditwheel
+    $PY -m pip install auditwheel
     auditwheel repair dist/*.whl
     rm -rf dist/*.whl
     mv wheelhouse/*.whl dist/
 fi
 
 # $PY -m twine upload --non-interactive -u '__token__' -p $PYPI_TOKEN dist/*
-"$PY" -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/* --verbose
+$PY -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/* --verbose
