861c782b7ce67b15a1b475e35edd1f772a639ef1
Mads Ynddal <mads-git@ynddal.dk>
2020-04-13 23:41:11 +0200

    idsf:

--- .deploy.sh
+++ .deploy.sh
@@ -4,21 +4,21 @@ set -e
 
 if [[ "$OS" == "Windows_NT" && "$MSYS" == "" ]]; then
     echo "Native Windows"
-    PY=/c/Program Files/Python37/python.exe
+    PY='/c/Program Files/Python37/python.exe'
 else
     echo "Unix-like"
     PY=python3
 fi
 
-$PY -m pip install wheel twine
-$PY setup.py sdist bdist_wheel
+"$PY" -m pip install wheel twine
+"$PY" setup.py sdist bdist_wheel
 
 if [ "$PLAT" = "manylinux2014_x86_64" ]; then
-    $PY -m pip install auditwheel
+    "$PY" -m pip install auditwheel
     auditwheel repair dist/*.whl
     rm -rf dist/*.whl
     mv wheelhouse/*.whl dist/
 fi
 
-# $PY -m twine upload --non-interactive -u '__token__' -p $PYPI_TOKEN dist/*
-$PY -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/* --verbose
+# "$PY" -m twine upload --non-interactive -u '__token__' -p $PYPI_TOKEN dist/*
+"$PY" -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/* --verbose
