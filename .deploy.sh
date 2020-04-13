615cbf9dd4ddbd4957a9b661c3e96e5a60fee549
Mads Ynddal <mads-git@ynddal.dk>
2020-04-13 13:22:25 +0200

    New API

--- .deploy.sh
+++ .deploy.sh
@@ -8,4 +8,4 @@ python3 -m pip install wheel twine
 
 python3 setup.py sdist bdist_wheel
 # python3 -m twine upload --non-interactive -u '__token__' -p $PYPI_TOKEN dist/*
-python3 -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/*
+python3 -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/* --verbose
