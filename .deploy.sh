5f3936249a92d626b5a47d063bbe8aba9cb34b67
Mads Ynddal <mads-git@ynddal.dk>
2020-04-13 12:49:20 +0200

    Twine hweel

--- .deploy.sh
+++ .deploy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# /c/Program\ Files/Python37/python.exe -m pip install wheel twine
+
+python3 -m pip install wheel twine
+
 python3 setup.py sdist bdist_wheel
 # python3 -m twine upload --non-interactive -u '__token__' -p $PYPI_TOKEN dist/*
 python3 -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ -u '__token__' -p $PYPI_TOKEN_TEST dist/*
