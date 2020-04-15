55581e67865f7ff428afe0345068fe46c553100a
Mads Ynddal <mads-git@ynddal.dk>
2020-04-15 20:04:13 +0200

    v0.9.12

--- setup.py
+++ setup.py
@@ -271,7 +271,7 @@ except FileNotFoundError:
 
 setup(
     name="pyboy",
-    version="0.9.11",
+    version="0.9.12",
     packages=find_packages(),
     author="Mads Ynddal",
     author_email="mads-pyboy@ynddal.dk",
