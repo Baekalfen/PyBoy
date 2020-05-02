c107d2ec26a30baffc78bbfbe550a69c7d0df5c8
Mads Ynddal <mads-git@ynddal.dk>
2020-05-02 21:01:22 +0200

    xdist2

--- setup.py
+++ setup.py
@@ -95,8 +95,8 @@ class PyTest(test):
                     sys.exit(return_code)
 
         import pytest
-        # args = ["tests/", f"-n{cpu_count()}", "-v", "--dist=loadfile"]
-        args = ["tests/", "-v", "-x"] # No multithreading, fail fast
+        args = ["tests/", f"-n{cpu_count()}", "-v", "--dist=loadfile"]
+        # args = ["tests/", "-v", "-x"] # No multithreading, fail fast
         if codecov: # TODO: There's probably a more correct way to read the argv flags
             args += ["--cov=./"]
         sys.exit(pytest.main(args))
