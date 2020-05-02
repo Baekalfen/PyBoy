bef2709da11b9d2a0310a3fb993e79e3b2e0d71d
Mads Ynddal <mads-github@ynddal.dk>
2020-05-02 23:03:40 +0200

    Pull Request testing + mooneye + acid + blargg (#150)
    * Added mooneye tests
    
    * Updated tests
    
    * New mooneye default results
    
    * Blargg tests
    
    * Fixed zip
    
    * test
    
    * xdist
    
    * xdist2
    
    * xdist3
    
    * skip rom
    
    * Added acid tests, fixed download race-condition
    
    * Travis support for pull requests

---
+++ tests/test_acid.py
@@ -0,0 +1,48 @@
+#
+# License: See LICENSE.md file
+# GitHub: https://github.com/Baekalfen/PyBoy
+#
+
+import os.path
+import urllib.request
+from pathlib import Path
+
+import PIL
+from pyboy import PyBoy
+
+OVERWRITE_PNGS = False
+
+
+def test_dmg_acid():
+    # Has to be in here. Otherwise all test workers will import the file, and cause an error.
+    dmg_acid_file = "dmg_acid2.gb"
+    if not os.path.isfile(dmg_acid_file):
+        dmg_acid_data = urllib.request.urlopen(
+            "https://github.com/mattcurrie/dmg-acid2/releases/download/v1.0/dmg-acid2.gb"
+        ).read()
+        with open(dmg_acid_file, "wb") as rom_file:
+            rom_file.write(dmg_acid_data)
+
+    pyboy = PyBoy(dmg_acid_file, window_type="headless")
+    pyboy.set_emulation_speed(0)
+    for _ in range(59):
+        pyboy.tick()
+
+    for _ in range(20):
+        pyboy.tick()
+
+    png_path = Path(f"test_results/{dmg_acid_file}.png")
+    image = pyboy.botsupport_manager().screen().screen_image()
+    if OVERWRITE_PNGS:
+        png_path.parents[0].mkdir(parents=True, exist_ok=True)
+        image.save(png_path)
+    else:
+        old_image = PIL.Image.open(png_path)
+        diff = PIL.ImageChops.difference(image, old_image)
+        if diff.getbbox() and not os.environ.get("TEST_CI"):
+            image.show()
+            old_image.show()
+            diff.show()
+        assert not diff.getbbox(), f"Images are different! {dmg_acid_file}"
+
+    pyboy.stop(save=False)
--- tests/test_blargg.py
+++ tests/test_blargg.py
@@ -3,13 +3,16 @@
 # GitHub: https://github.com/Baekalfen/PyBoy
 #
 
+import io
 import json
 import os.path
 import platform
 import sys
 import time
+import urllib.request
+from pathlib import Path
+from zipfile import ZipFile
 
-import pytest
 from pyboy import PyBoy
 
 if platform.python_implementation() == "PyPy":
@@ -17,10 +20,12 @@ if platform.python_implementation() == "PyPy":
 else:
     timeout = 5
 
+OVERWRITE_JSON = False
+
 
 def run_rom(args):
     rom, frame_limit = args
-    pyboy = PyBoy(rom, window_type="dummy", disable_input=True)
+    pyboy = PyBoy(rom, window_type="dummy")
     pyboy.set_emulation_speed(0)
     serial_output = ""
     t = time.time()
@@ -50,87 +55,85 @@ def run_rom(args):
     return result
 
 
-@pytest.mark.skipif(not os.path.isdir("ROMs/BlarggROMs"), reason="ROM not present")
 def test_blarggs():
+    # Has to be in here. Otherwise all test workers will import the file, and cause an error.
+    blargg_dir = Path("blargg")
+    if not os.path.isdir(blargg_dir):
+        for name in [
+            "cgb_sound",
+            "cpu_instrs",
+            "dmg_sound",
+            "halt_bug",
+            "instr_timing",
+            "interrupt_time",
+            "mem_timing-2",
+            "mem_timing",
+            "oam_bug",
+        ]:
+            blargg_data = io.BytesIO(
+                urllib.request.urlopen(f"https://gbdev.gg8.se/files/roms/blargg-gb-tests/{name}.zip").read()
+            )
+            with ZipFile(blargg_data) as _zip:
+                _zip.extractall(blargg_dir)
+
     test_roms = [
-        ("ROMs/BlarggROMs/instr_timing/instr_timing.gb", 1500),
-        ("ROMs/BlarggROMs/mem_timing/individual/02-write_timing.gb", 500),
-        ("ROMs/BlarggROMs/mem_timing/individual/01-read_timing.gb", 500),
-        ("ROMs/BlarggROMs/mem_timing/individual/03-modify_timing.gb", 500),
-        ("ROMs/BlarggROMs/mem_timing/mem_timing.gb", 430),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/02-interrupts.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/09-op r,r.gb", 1000),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/11-op a,(hl).gb", 1500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/10-bit ops.gb", 1500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/04-op r,imm.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/01-special.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/06-ld r,r.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/03-op sp,hl.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/08-misc instrs.gb", 500),
-        ("ROMs/BlarggROMs/cpu_instrs/individual/05-op rp.gb", 1000),
-        ("ROMs/BlarggROMs/cpu_instrs/cpu_instrs.gb", 5000),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/11-regs after power.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/10-wave trigger while on.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/09-wave read while on.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/02-len ctr.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/01-registers.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/07-len sweep period sync.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/04-sweep.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/08-len ctr during power.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/05-sweep details.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/03-trigger.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/06-overflow on trigger.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/rom_singles/12-wave.gb", -1),
-        # ("ROMs/BlarggROMs/cgb_sound/cgb_sound.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/8-instr_effect.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/7-timing_effect.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/4-scanline_timing.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/2-causes.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/3-non_causes.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/6-timing_no_bug.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/1-lcd_sync.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/rom_singles/5-timing_bug.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug/oam_bug.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/8-instr_effect.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/7-timing_effect.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/4-scanline_timing.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/2-causes.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/3-non_causes.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/6-timing_no_bug.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/1-lcd_sync.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/rom_singles/5-timing_bug.gb", -1),
-        ("ROMs/BlarggROMs/oam_bug-2/oam_bug.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/11-regs after power.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/10-wave trigger while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/12-wave write while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/09-wave read while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/02-len ctr.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/01-registers.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/07-len sweep period sync.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/04-sweep.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/08-len ctr during power.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/05-sweep details.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/03-trigger.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/rom_singles/06-overflow on trigger.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound/dmg_sound.gb", -1),
-        ("ROMs/BlarggROMs/mem_timing-2/rom_singles/02-write_timing.gb", -1),
-        ("ROMs/BlarggROMs/mem_timing-2/rom_singles/01-read_timing.gb", -1),
-        ("ROMs/BlarggROMs/mem_timing-2/rom_singles/03-modify_timing.gb", -1),
-        ("ROMs/BlarggROMs/mem_timing-2/mem_timing.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/11-regs after power.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/10-wave trigger while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/12-wave write while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/09-wave read while on.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/02-len ctr.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/01-registers.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/07-len sweep period sync.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/04-sweep.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/08-len ctr during power.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/05-sweep details.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/03-trigger.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/rom_singles/06-overflow on trigger.gb", -1),
-        ("ROMs/BlarggROMs/dmg_sound-2/dmg_sound.gb", -1),
+        # ("blargg/cgb_sound/cgb_sound.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/01-registers.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/02-len ctr.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/03-trigger.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/04-sweep.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/05-sweep details.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/06-overflow on trigger.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/07-len sweep period sync.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/08-len ctr during power.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/09-wave read while on.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/10-wave trigger while on.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/11-regs after power.gb", -1),
+        # ("blargg/cgb_sound/rom_singles/12-wave.gb", -1),
+        ("blargg/cpu_instrs/cpu_instrs.gb", 5000),
+        ("blargg/cpu_instrs/individual/01-special.gb", 500),
+        ("blargg/cpu_instrs/individual/02-interrupts.gb", 500),
+        ("blargg/cpu_instrs/individual/03-op sp,hl.gb", 500),
+        ("blargg/cpu_instrs/individual/04-op r,imm.gb", 500),
+        ("blargg/cpu_instrs/individual/05-op rp.gb", 1000),
+        ("blargg/cpu_instrs/individual/06-ld r,r.gb", 500),
+        ("blargg/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb", 500),
+        ("blargg/cpu_instrs/individual/08-misc instrs.gb", 500),
+        ("blargg/cpu_instrs/individual/09-op r,r.gb", 1000),
+        ("blargg/cpu_instrs/individual/10-bit ops.gb", 1500),
+        ("blargg/cpu_instrs/individual/11-op a,(hl).gb", 1500),
+        ("blargg/dmg_sound/dmg_sound.gb", -1),
+        ("blargg/dmg_sound/rom_singles/01-registers.gb", -1),
+        ("blargg/dmg_sound/rom_singles/02-len ctr.gb", -1),
+        ("blargg/dmg_sound/rom_singles/03-trigger.gb", -1),
+        ("blargg/dmg_sound/rom_singles/04-sweep.gb", -1),
+        ("blargg/dmg_sound/rom_singles/05-sweep details.gb", -1),
+        ("blargg/dmg_sound/rom_singles/06-overflow on trigger.gb", -1),
+        ("blargg/dmg_sound/rom_singles/07-len sweep period sync.gb", -1),
+        ("blargg/dmg_sound/rom_singles/08-len ctr during power.gb", -1),
+        ("blargg/dmg_sound/rom_singles/09-wave read while on.gb", -1),
+        ("blargg/dmg_sound/rom_singles/10-wave trigger while on.gb", -1),
+        ("blargg/dmg_sound/rom_singles/11-regs after power.gb", -1),
+        ("blargg/dmg_sound/rom_singles/12-wave write while on.gb", -1),
+        ("blargg/instr_timing/instr_timing.gb", 1500),
+        ("blargg/interrupt_time/interrupt_time.gb", 100),
+        ("blargg/mem_timing/individual/01-read_timing.gb", 500),
+        ("blargg/mem_timing/individual/02-write_timing.gb", 500),
+        ("blargg/mem_timing/individual/03-modify_timing.gb", 500),
+        ("blargg/mem_timing/mem_timing.gb", 430),
+        ("blargg/mem_timing-2/mem_timing.gb", -1),
+        ("blargg/mem_timing-2/rom_singles/01-read_timing.gb", -1),
+        ("blargg/mem_timing-2/rom_singles/02-write_timing.gb", -1),
+        ("blargg/mem_timing-2/rom_singles/03-modify_timing.gb", -1),
+        ("blargg/oam_bug/oam_bug.gb", -1),
+        ("blargg/oam_bug/rom_singles/1-lcd_sync.gb", -1),
+        ("blargg/oam_bug/rom_singles/2-causes.gb", -1),
+        ("blargg/oam_bug/rom_singles/3-non_causes.gb", -1),
+        ("blargg/oam_bug/rom_singles/4-scanline_timing.gb", -1),
+        ("blargg/oam_bug/rom_singles/5-timing_bug.gb", -1),
+        ("blargg/oam_bug/rom_singles/6-timing_no_bug.gb", -1),
+        ("blargg/oam_bug/rom_singles/7-timing_effect.gb", -1),
+        ("blargg/oam_bug/rom_singles/8-instr_effect.gb", -1),
     ]
 
     if os.environ.get("TEST_CI") or sys.platform == "win32":
@@ -140,27 +143,29 @@ def test_blarggs():
         pool = mp.Pool(mp.cpu_count())
         results = pool.map(run_rom, test_roms)
 
-    blargg_json = "blargg.json"
+    blargg_json = "test_results/blargg.json"
 
-    if os.path.isfile(blargg_json):
-        with open(blargg_json, "r") as f:
-            old_blargg = json.load(f)
+    if OVERWRITE_JSON:
+        with open(blargg_json, "w") as f:
+            json.dump(dict(zip([x for x, _ in test_roms], results)), f)
 
-            assert len(old_blargg) == len(test_roms)
+        blargg_md = "../PyBoy.wiki/blargg.md"
+        if os.path.isdir(blargg_md):
+            with open(blargg_md, "w") as f:
+                f.write("# Test results for Blargg's test ROMs\n")
+                f.write("|ROM|Result|\n")
+                f.write("|---|---|\n")
+                for (rom, _), res in zip(test_roms, results):
+                    f.write("|%s|%s|\n" % (rom, res.replace("\n", " ").rstrip(":")))
+    else:
+        if os.path.isfile(blargg_json):
+            with open(blargg_json, "r") as f:
+                old_blargg = json.load(f)
 
-            for (rom, _) in test_roms:
-                assert old_blargg.get(rom)
+                assert len(old_blargg) == len(test_roms)
 
-            for (rom, _), res in zip(test_roms, results):
-                assert old_blargg[rom] == res, f"Outputs don't match for {rom}"
+                for (rom, _) in test_roms:
+                    assert old_blargg.get(rom)
 
-    # with open(blargg_json, "w") as f:
-    #     json.dump(dict(zip([x for x, _ in test_roms], results)), f)
-
-    if not os.environ.get("TEST_CI"):
-        with open("../PyBoy.wiki/blargg.md", "w") as f:
-            f.write("# Test results for Blargg's test ROMs\n")
-            f.write("|ROM|Result|\n")
-            f.write("|---|---|\n")
-            for (rom, _), res in zip(test_roms, results):
-                f.write("|%s|%s|\n" % (rom, res.replace("\n", " ").rstrip(":")))
+                for (rom, _), res in zip(test_roms, results):
+                    assert old_blargg[rom] == res, f"Outputs don't match for {rom}"
