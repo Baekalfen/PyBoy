9f8da15cceefeffd374f987b9e8f77b59c43ddb3
Mads Ynddal <mads-github@ynddal.dk>
2020-10-06 23:17:48 +0200

    Optimize bootrom and add sound effect (#184)

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -7,13 +7,13 @@ import os
 
 import pytest
 from tests.test_replay import replay
-from tests.utils import default_rom
+from tests.utils import BOOTROM_FRAMES_UNTIL_LOGO, default_rom
 
 replay_file = "tests/replays/default_rom.replay"
 
 
 def test_headless():
-    replay(default_rom, replay_file, "headless", bootrom_file=None)
+    replay(default_rom, replay_file, "headless", bootrom_file=None, padding_frames=BOOTROM_FRAMES_UNTIL_LOGO)
 
 
 def test_dummy():
@@ -22,9 +22,9 @@ def test_dummy():
 
 @pytest.mark.skipif(os.environ.get("TEST_NO_UI"), reason="Skipping test, as there is no UI")
 def test_sdl2():
-    replay(default_rom, replay_file, "SDL2", bootrom_file=None)
+    replay(default_rom, replay_file, "SDL2", bootrom_file=None, padding_frames=BOOTROM_FRAMES_UNTIL_LOGO)
 
 
 @pytest.mark.skipif(os.environ.get("TEST_NO_UI"), reason="Skipping test, as there is no UI")
 def test_opengl():
-    replay(default_rom, replay_file, "OpenGL", bootrom_file=None)
+    replay(default_rom, replay_file, "OpenGL", bootrom_file=None, padding_frames=BOOTROM_FRAMES_UNTIL_LOGO)
