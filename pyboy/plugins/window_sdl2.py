74847d9247491993a95b19e5c246d5b1a72aadb3
Kristian Sims <kristian.sims@gmail.com>
2022-06-13 23:05:31 +0200

    Use SDL2 without Cython compilation

--- pyboy/plugins/window_sdl2.py
+++ pyboy/plugins/window_sdl2.py
@@ -6,97 +6,100 @@
 import logging
 from time import perf_counter
 
-import sdl2
-import sdl2.ext
-from pyboy.logger import logger
 from pyboy.plugins.base_plugin import PyBoyWindowPlugin
 from pyboy.utils import WindowEvent, WindowEventMouse
+
+try:
+    import sdl2
+    from sdl2.ext import get_events
+except ImportError:
+    sdl2 = None
 
 logger = logging.getLogger(__name__)
 
 ROWS, COLS = 144, 160
 
-try:
-    from cython import compiled
-    cythonmode = compiled
-except ImportError:
-    cythonmode = False
-
 # https://wiki.libsdl.org/SDL_Scancode#Related_Enumerations
 # yapf: disable
-KEY_DOWN = {
-    sdl2.SDLK_UP        : WindowEvent.PRESS_ARROW_UP,
-    sdl2.SDLK_DOWN      : WindowEvent.PRESS_ARROW_DOWN,
-    sdl2.SDLK_RIGHT     : WindowEvent.PRESS_ARROW_RIGHT,
-    sdl2.SDLK_LEFT      : WindowEvent.PRESS_ARROW_LEFT,
-    sdl2.SDLK_a         : WindowEvent.PRESS_BUTTON_A,
-    sdl2.SDLK_s         : WindowEvent.PRESS_BUTTON_B,
-    sdl2.SDLK_RETURN    : WindowEvent.PRESS_BUTTON_START,
-    sdl2.SDLK_BACKSPACE : WindowEvent.PRESS_BUTTON_SELECT,
-    sdl2.SDLK_SPACE     : WindowEvent.PRESS_SPEED_UP,
-    sdl2.SDLK_COMMA     : WindowEvent.PRESS_REWIND_BACK,
-    sdl2.SDLK_PERIOD    : WindowEvent.PRESS_REWIND_FORWARD,
-    sdl2.SDLK_j         : WindowEvent.DEBUG_MEMORY_SCROLL_DOWN,
-    sdl2.SDLK_k         : WindowEvent.DEBUG_MEMORY_SCROLL_UP,
-    sdl2.SDLK_LSHIFT    : WindowEvent.MOD_SHIFT_ON,
-    sdl2.SDLK_RSHIFT    : WindowEvent.MOD_SHIFT_ON,
-}
+if sdl2:
+    KEY_DOWN = {
+        sdl2.SDLK_UP        : WindowEvent.PRESS_ARROW_UP,
+        sdl2.SDLK_DOWN      : WindowEvent.PRESS_ARROW_DOWN,
+        sdl2.SDLK_RIGHT     : WindowEvent.PRESS_ARROW_RIGHT,
+        sdl2.SDLK_LEFT      : WindowEvent.PRESS_ARROW_LEFT,
+        sdl2.SDLK_a         : WindowEvent.PRESS_BUTTON_A,
+        sdl2.SDLK_s         : WindowEvent.PRESS_BUTTON_B,
+        sdl2.SDLK_RETURN    : WindowEvent.PRESS_BUTTON_START,
+        sdl2.SDLK_BACKSPACE : WindowEvent.PRESS_BUTTON_SELECT,
+        sdl2.SDLK_SPACE     : WindowEvent.PRESS_SPEED_UP,
+        sdl2.SDLK_COMMA     : WindowEvent.PRESS_REWIND_BACK,
+        sdl2.SDLK_PERIOD    : WindowEvent.PRESS_REWIND_FORWARD,
+        sdl2.SDLK_j         : WindowEvent.DEBUG_MEMORY_SCROLL_DOWN,
+        sdl2.SDLK_k         : WindowEvent.DEBUG_MEMORY_SCROLL_UP,
+        sdl2.SDLK_LSHIFT    : WindowEvent.MOD_SHIFT_ON,
+        sdl2.SDLK_RSHIFT    : WindowEvent.MOD_SHIFT_ON,
+    }
 
-KEY_UP = {
-    sdl2.SDLK_UP        : WindowEvent.RELEASE_ARROW_UP,
-    sdl2.SDLK_DOWN      : WindowEvent.RELEASE_ARROW_DOWN,
-    sdl2.SDLK_RIGHT     : WindowEvent.RELEASE_ARROW_RIGHT,
-    sdl2.SDLK_LEFT      : WindowEvent.RELEASE_ARROW_LEFT,
-    sdl2.SDLK_a         : WindowEvent.RELEASE_BUTTON_A,
-    sdl2.SDLK_s         : WindowEvent.RELEASE_BUTTON_B,
-    sdl2.SDLK_RETURN    : WindowEvent.RELEASE_BUTTON_START,
-    sdl2.SDLK_BACKSPACE : WindowEvent.RELEASE_BUTTON_SELECT,
-    sdl2.SDLK_z         : WindowEvent.STATE_SAVE,
-    sdl2.SDLK_x         : WindowEvent.STATE_LOAD,
-    sdl2.SDLK_SPACE     : WindowEvent.RELEASE_SPEED_UP,
-    sdl2.SDLK_p         : WindowEvent.PAUSE_TOGGLE,
-    sdl2.SDLK_i         : WindowEvent.SCREEN_RECORDING_TOGGLE,
-    sdl2.SDLK_o         : WindowEvent.SCREENSHOT_RECORD,
-    sdl2.SDLK_ESCAPE    : WindowEvent.QUIT,
-    sdl2.SDLK_COMMA     : WindowEvent.RELEASE_REWIND_BACK,
-    sdl2.SDLK_PERIOD    : WindowEvent.RELEASE_REWIND_FORWARD,
-    sdl2.SDLK_LSHIFT    : WindowEvent.MOD_SHIFT_OFF,
-    sdl2.SDLK_RSHIFT    : WindowEvent.MOD_SHIFT_OFF,
-    sdl2.SDLK_F11       : WindowEvent.FULL_SCREEN_TOGGLE,
-}
+    KEY_UP = {
+        sdl2.SDLK_UP        : WindowEvent.RELEASE_ARROW_UP,
+        sdl2.SDLK_DOWN      : WindowEvent.RELEASE_ARROW_DOWN,
+        sdl2.SDLK_RIGHT     : WindowEvent.RELEASE_ARROW_RIGHT,
+        sdl2.SDLK_LEFT      : WindowEvent.RELEASE_ARROW_LEFT,
+        sdl2.SDLK_a         : WindowEvent.RELEASE_BUTTON_A,
+        sdl2.SDLK_s         : WindowEvent.RELEASE_BUTTON_B,
+        sdl2.SDLK_RETURN    : WindowEvent.RELEASE_BUTTON_START,
+        sdl2.SDLK_BACKSPACE : WindowEvent.RELEASE_BUTTON_SELECT,
+        sdl2.SDLK_z         : WindowEvent.STATE_SAVE,
+        sdl2.SDLK_x         : WindowEvent.STATE_LOAD,
+        sdl2.SDLK_SPACE     : WindowEvent.RELEASE_SPEED_UP,
+        sdl2.SDLK_p         : WindowEvent.PAUSE_TOGGLE,
+        sdl2.SDLK_i         : WindowEvent.SCREEN_RECORDING_TOGGLE,
+        sdl2.SDLK_o         : WindowEvent.SCREENSHOT_RECORD,
+        sdl2.SDLK_ESCAPE    : WindowEvent.QUIT,
+        sdl2.SDLK_COMMA     : WindowEvent.RELEASE_REWIND_BACK,
+        sdl2.SDLK_PERIOD    : WindowEvent.RELEASE_REWIND_FORWARD,
+        sdl2.SDLK_LSHIFT    : WindowEvent.MOD_SHIFT_OFF,
+        sdl2.SDLK_RSHIFT    : WindowEvent.MOD_SHIFT_OFF,
+        sdl2.SDLK_F11       : WindowEvent.FULL_SCREEN_TOGGLE,
+    }
 
-CONTROLLER_DOWN = {
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_UP       : WindowEvent.PRESS_ARROW_UP,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_DOWN     : WindowEvent.PRESS_ARROW_DOWN,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_RIGHT    : WindowEvent.PRESS_ARROW_RIGHT,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_LEFT     : WindowEvent.PRESS_ARROW_LEFT,
-    sdl2.SDL_CONTROLLER_BUTTON_B             : WindowEvent.PRESS_BUTTON_A,
-    sdl2.SDL_CONTROLLER_BUTTON_A             : WindowEvent.PRESS_BUTTON_B,
-    sdl2.SDL_CONTROLLER_BUTTON_START         : WindowEvent.PRESS_BUTTON_START,
-    sdl2.SDL_CONTROLLER_BUTTON_BACK          : WindowEvent.PRESS_BUTTON_SELECT,
-    sdl2.SDL_CONTROLLER_BUTTON_GUIDE         : WindowEvent.PRESS_SPEED_UP,
-}
+    CONTROLLER_DOWN = {
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_UP       : WindowEvent.PRESS_ARROW_UP,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_DOWN     : WindowEvent.PRESS_ARROW_DOWN,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_RIGHT    : WindowEvent.PRESS_ARROW_RIGHT,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_LEFT     : WindowEvent.PRESS_ARROW_LEFT,
+        sdl2.SDL_CONTROLLER_BUTTON_B             : WindowEvent.PRESS_BUTTON_A,
+        sdl2.SDL_CONTROLLER_BUTTON_A             : WindowEvent.PRESS_BUTTON_B,
+        sdl2.SDL_CONTROLLER_BUTTON_START         : WindowEvent.PRESS_BUTTON_START,
+        sdl2.SDL_CONTROLLER_BUTTON_BACK          : WindowEvent.PRESS_BUTTON_SELECT,
+        sdl2.SDL_CONTROLLER_BUTTON_GUIDE         : WindowEvent.PRESS_SPEED_UP,
+    }
 
-CONTROLLER_UP = {
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_UP       : WindowEvent.RELEASE_ARROW_UP,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_DOWN     : WindowEvent.RELEASE_ARROW_DOWN,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_RIGHT    : WindowEvent.RELEASE_ARROW_RIGHT,
-    sdl2.SDL_CONTROLLER_BUTTON_DPAD_LEFT     : WindowEvent.RELEASE_ARROW_LEFT,
-    sdl2.SDL_CONTROLLER_BUTTON_B             : WindowEvent.RELEASE_BUTTON_A,
-    sdl2.SDL_CONTROLLER_BUTTON_A             : WindowEvent.RELEASE_BUTTON_B,
-    sdl2.SDL_CONTROLLER_BUTTON_START         : WindowEvent.RELEASE_BUTTON_START,
-    sdl2.SDL_CONTROLLER_BUTTON_BACK          : WindowEvent.RELEASE_BUTTON_SELECT,
-    sdl2.SDL_CONTROLLER_BUTTON_LEFTSHOULDER  : WindowEvent.STATE_SAVE,
-    sdl2.SDL_CONTROLLER_BUTTON_RIGHTSHOULDER : WindowEvent.STATE_LOAD,
-    sdl2.SDL_CONTROLLER_BUTTON_GUIDE         : WindowEvent.RELEASE_SPEED_UP,
-}
+    CONTROLLER_UP = {
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_UP       : WindowEvent.RELEASE_ARROW_UP,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_DOWN     : WindowEvent.RELEASE_ARROW_DOWN,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_RIGHT    : WindowEvent.RELEASE_ARROW_RIGHT,
+        sdl2.SDL_CONTROLLER_BUTTON_DPAD_LEFT     : WindowEvent.RELEASE_ARROW_LEFT,
+        sdl2.SDL_CONTROLLER_BUTTON_B             : WindowEvent.RELEASE_BUTTON_A,
+        sdl2.SDL_CONTROLLER_BUTTON_A             : WindowEvent.RELEASE_BUTTON_B,
+        sdl2.SDL_CONTROLLER_BUTTON_START         : WindowEvent.RELEASE_BUTTON_START,
+        sdl2.SDL_CONTROLLER_BUTTON_BACK          : WindowEvent.RELEASE_BUTTON_SELECT,
+        sdl2.SDL_CONTROLLER_BUTTON_LEFTSHOULDER  : WindowEvent.STATE_SAVE,
+        sdl2.SDL_CONTROLLER_BUTTON_RIGHTSHOULDER : WindowEvent.STATE_LOAD,
+        sdl2.SDL_CONTROLLER_BUTTON_GUIDE         : WindowEvent.RELEASE_SPEED_UP,
+    }
+else:
+    KEY_DOWN = {}
+    KEY_UP = {}
+    CONTROLLER_DOWN = {}
+    CONTROLLER_UP = {}
 # yapf: enable
 
 
 def sdl2_event_pump(events):
     global _sdlcontroller
     # Feed events into the loop
-    for event in sdl2.ext.get_events():
+    for event in get_events():
         if event.type == sdl2.SDL_QUIT:
             events.append(WindowEvent(WindowEvent.QUIT))
         elif event.type == sdl2.SDL_KEYDOWN:
@@ -185,10 +188,20 @@ class WindowSDL2(PyBoyWindowPlugin):
         return events
 
     def post_tick(self):
-        self._update_display()
+        sdl2.SDL_UpdateTexture(self._sdltexturebuffer, None, self.renderer._screenbuffer_ptr, COLS * 4)
+        sdl2.SDL_RenderCopy(self._sdlrenderer, self._sdltexturebuffer, None, None)
+        sdl2.SDL_RenderPresent(self._sdlrenderer)
+        sdl2.SDL_RenderClear(self._sdlrenderer)
 
     def enabled(self):
-        return self.pyboy_argv.get("window_type") == "SDL2" or self.pyboy_argv.get("window_type") is None
+        if self.pyboy_argv.get("window_type") in ("SDL2", None):
+            if not sdl2:
+                logger.error("Failed to import sdl2, needed for sdl2 window")
+                return False # Disable, or raise exception?
+            else:
+                return True
+        else:
+            return False
 
     def frame_limiter(self, speed):
         self._ftime += 1.0 / (60.0*speed)
@@ -203,23 +216,5 @@ class WindowSDL2(PyBoyWindowPlugin):
     def stop(self):
         sdl2.SDL_DestroyWindow(self._window)
         for _ in range(10): # At least 2 to close
-            sdl2.ext.get_events()
+            get_events()
         sdl2.SDL_Quit()
-
-
-# Unfortunately CPython/PyPy code has to be hidden in an exec call to
-# prevent Cython from trying to parse it. This block provides the
-# functions that are otherwise implemented as inlined cdefs in the pxd
-if not cythonmode:
-    exec(
-        """
-def _update_display(self):
-    # self.renderer._screenbuffer[0]._pypy_raw_address()
-    sdl2.SDL_UpdateTexture(self._sdltexturebuffer, None, self.renderer._screenbuffer_ptr, COLS*4)
-    sdl2.SDL_RenderCopy(self._sdlrenderer, self._sdltexturebuffer, None, None)
-    sdl2.SDL_RenderPresent(self._sdlrenderer)
-    sdl2.SDL_RenderClear(self._sdlrenderer)
-
-WindowSDL2._update_display = _update_display
-""", globals(), locals()
-    )
