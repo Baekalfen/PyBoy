d00b6100d3e2657e5176550e1e8af151ff1e18cd
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2022-06-19 22:30:35 +0200

    Replace links in README for PyPi

--- setup.py
+++ setup.py
@@ -196,7 +196,7 @@ if CYTHON and "clean" not in sys.argv:
 try:
     this_directory = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-        long_description = f.read()
+        long_description = f.read().replace("README/", "https://github.com/Baekalfen/PyBoy/raw/master/README/")
 except FileNotFoundError:
     print("README.md not found")
     long_description = ""
