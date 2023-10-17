8b7c054b4d45a37f403bcfd3ed5bab6db041ef29
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2023-10-17 21:54:26 +0200

    OpenGL catch attribute error

--- pyboy/plugins/window_open_gl.py
+++ pyboy/plugins/window_open_gl.py
@@ -6,6 +6,7 @@
 import logging
 
 import numpy as np
+
 from pyboy.logger import logger
 from pyboy.plugins.base_plugin import PyBoyWindowPlugin
 from pyboy.utils import WindowEvent
@@ -14,18 +15,14 @@ logger = logging.getLogger(__name__)
 
 try:
     import OpenGL.GLUT.freeglut
-    from OpenGL.GL import (
-        GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, glClear, glDrawPixels, glFlush,
-        glPixelZoom
-    )
-    # from OpenGL.GLU import *
-    from OpenGL.GLUT import (
-        GLUT_KEY_DOWN, GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_RGBA, GLUT_SINGLE, glutCreateWindow,
-        glutDestroyWindow, glutDisplayFunc, glutGetWindow, glutInit, glutInitDisplayMode, glutInitWindowSize,
-        glutKeyboardFunc, glutKeyboardUpFunc, glutReshapeFunc, glutSetWindowTitle, glutSpecialFunc, glutSpecialUpFunc
-    )
+    from OpenGL.GL import (GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, glClear,
+                           glDrawPixels, glFlush, glPixelZoom)
+    from OpenGL.GLUT import (GLUT_KEY_DOWN, GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_RGBA, GLUT_SINGLE,
+                             glutCreateWindow, glutDestroyWindow, glutDisplayFunc, glutGetWindow, glutInit,
+                             glutInitDisplayMode, glutInitWindowSize, glutKeyboardFunc, glutKeyboardUpFunc,
+                             glutReshapeFunc, glutSetWindowTitle, glutSpecialFunc, glutSpecialUpFunc)
     opengl_enabled = True
-except ImportError:
+except (ImportError, AttributeError):
     opengl_enabled = False
 
 ROWS, COLS = 144, 160
