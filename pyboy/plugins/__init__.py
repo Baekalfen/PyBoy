642b5c2cac7563453c2f26f3d705c00cea423586
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2023-10-27 23:03:00 +0200

    Refactor game_area code for v2.0.0

--- pyboy/plugins/__init__.py
+++ pyboy/plugins/__init__.py
@@ -8,18 +8,18 @@ Plugins that extend PyBoy's functionality. The only publicly exposed, are the ga
 
 __pdoc__ = {
     # docs exclude
-    "window_open_gl": False,
-    "window_sdl2": False,
-    "auto_pause": False,
     "disable_input": False,
     "rewind": False,
-    "manager_gen": False,
-    "debug": False,
-    "record_replay": False,
-    "window_null": False,
-    "manager": False,
-    "debug_prompt": False,
+    "window_sdl2": False,
     "screenshot_recorder": False,
+    "debug_prompt": False,
     "screen_recorder": False,
+    "debug": False,
+    "manager": False,
+    "record_replay": False,
+    "manager_gen": False,
+    "window_open_gl": False,
+    "auto_pause": False,
+    "window_null": False,
     # docs exclude end
 }
--- tests/test_game_wrapper.py
+++ tests/test_game_wrapper.py
@@ -22,5 +22,57 @@ def test_game_wrapper_basics(default_rom):
 
     generic_wrapper = pyboy.game_wrapper
     assert generic_wrapper is not None
-    # pyboy.game_area()
+    pyboy.game_area()
+    pyboy.stop()
+
+
+def test_game_wrapper_mapping(default_rom):
+    pyboy = PyBoy(default_rom, window_type="null", debug=True)
+    pyboy.set_emulation_speed(0)
+    assert np.all(pyboy.game_area() == 256)
+
+    generic_wrapper = pyboy.game_wrapper
+    assert generic_wrapper is not None
+    pyboy.tick(5, True)
+    assert np.all(
+        pyboy.game_area()[8:11,
+                          8:13] == np.array([
+                              [1, 0, 1, 0, 0],
+                              [2, 3, 4, 5, 3],
+                              [0, 6, 0, 0, 6],
+                          ], dtype=np.uint32)
+    )
+
+    # List-based mapping, don't call tick
+    mapping = [x for x in range(384)] # 1:1 mapping
+    mapping[0] = 10
+    mapping[1] = 10
+    mapping[2] = 10
+    mapping[3] = 10
+    pyboy.game_area_mapping(mapping, 1000)
+    assert np.all(
+        pyboy.game_area()[8:11, 8:13] == np.array([
+            [10, 10, 10, 10, 10],
+            [10, 10, 4, 5, 10],
+            [10, 6, 10, 10, 6],
+        ],
+                                                  dtype=np.uint32)
+    )
+
+    # Array-based mapping, don't call tick
+    mapping = np.asarray(mapping)
+    mapping[0] = 1 # Map tile identifier 0 -> 1
+    mapping[1] = 1 # Map tile identifier 1 -> 1
+    mapping[2] = 1 # Map tile identifier 2 -> 1
+    mapping[3] = 1 # Map tile identifier 3 -> 1
+    pyboy.game_area_mapping(mapping, 1000)
+    assert np.all(
+        pyboy.game_area()[8:11,
+                          8:13] == np.array([
+                              [1, 1, 1, 1, 1],
+                              [1, 1, 4, 5, 1],
+                              [1, 6, 1, 1, 6],
+                          ], dtype=np.uint32)
+    )
+
     pyboy.stop()
