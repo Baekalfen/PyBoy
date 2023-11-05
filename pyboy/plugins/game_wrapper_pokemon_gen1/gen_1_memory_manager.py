19983906dc9f4eecf52271524530b45e78cd1ef1
Patrick McQuay <patrick.mcquay@gmail.com>
2023-11-04 20:07:17 -0400

    Create Pokemon class to wrap memory loading of all values of a pokemon in the party

--- pyboy/plugins/game_wrapper_pokemon_gen1/gen_1_memory_manager.py
+++ pyboy/plugins/game_wrapper_pokemon_gen1/gen_1_memory_manager.py
@@ -7,35 +7,127 @@ WRAM = (0xC000, 0xDFFF) # Unsure of where this range ACTUALLY ends
 HRAM = (0xFF80, 0xFFFE)
 
 # Pokemon locations in memory
-POKEMON_1_BASE = 0xD16B
-POKEMON_2_BASE = 0xD197
-POKEMON_3_BASE = 0xD1C3
-POKEMON_4_BASE = 0xD1EF
-POKEMON_5_BASE = 0xD21B
-POKEMON_6_BASE = 0xD247
+class PokemonBaseAddrs(Enum):
+    POKEMON_1 = 0xD16B
+    POKEMON_2 = 0xD197
+    POKEMON_3 = 0xD1C3
+    POKEMON_4 = 0xD1EF
+    POKEMON_5 = 0xD21B
+    POKEMON_6 = 0xD247
 
 # Offsets for Pokemon data access
 # Structure is (offset, num bytes)
-POKEMON_OFFSET = (0x0, 1) 
-POKEMON_CURRENT_HP_OFFSET = (0x1, 2)
-POKEMON_STATUS_OFFSET = (0x3, 1)
-POKEMON_TYPE_1_OFFSET = (0x4, 1)
-POKEMON_TYPE_2_OFFSET = (0x5, 1)
-POKEMON_CATCH_RATE_OFFSET = (0x6, 1)
-POKEMON_MOVE_1_OFFSET = (0x7, 1)
-POKEMON_MOVE_2_OFFSET = (0x8, 1)
-POKEMON_MOVE_3_OFFSET = (0x9, 1)
-POKEMON_MOVE_4_OFFSET = (0xA, 1)
-POKEMON_TRAINER_ID_OFFSET = (0xB, 2)
-POKEMON_EXPERIENCE_OFFSET = (0xD, 3)
+class PokemonOffsets(Enum):
+    ID = (0x0, 1) 
+    CURRENT_HP = (0x1, 2)
+    STATUS = (0x3, 1)
+    TYPE_1 = (0x4, 1)
+    TYPE_2 = (0x5, 1)
+    CATCH_RATE = (0x6, 1)
+    MOVE_1 = (0x7, 1)
+    MOVE_2 = (0x8, 1)
+    MOVE_3 = (0x9, 1)
+    MOVE_4 = (0xA, 1)
+    TRAINER_ID = (0xB, 2)
+    EXPERIENCE = (0xD, 3)
+    HP_EV = (0x11, 2)
+    ATTACK_EV = (0x13, 2)
+    DEFENSE_EV = (0x15, 2)
+    SPEED_EV = (0x17, 2)
+    SPECIAL_EV = (0x19, 2)
+    ATTACK_DEFENSE_IV = (0x1B, 1)
+    SPEED_SPECIAL_IV = (0x1C, 1)
+    PP_MOVE_1 = (0x1D, 1)
+    PP_MOVE_2 = (0x1E, 1)
+    PP_MOVE_3 = (0x1F, 1)
+    PP_MOVE_4 = (0x20, 1)
+    LEVEL = (0x21, 1)
+    MAX_HP = (0x22, 2)
+    ATTACK = (0x24, 2)
+    DEFENSE = (0x26, 2)
+    SPEED = (0x28, 2)
+    SPECIAL = (0x2A, 2)
 
-POKEMON_LEVEL_OFFSET = (0x21, 1)
+class Pokemon:
 
+    def __init__(self,
+                 pokemon_id,
+                 current_hp,
+                 status,
+                 type_1,
+                 type_2,
+                 catch_rate,
+                 move_1,
+                 move_2,
+                 move_3,
+                 move_4,
+                 trainer_id,
+                 experience,
+                 hp_ev,
+                 attack_ev,
+                 defense_ev,
+                 speed_ev,
+                 special_ev,
+                 attack_defense_iv,
+                 speed_special_iv,
+                 pp_move_1,
+                 pp_move_2,
+                 pp_move_3,
+                 pp_move_4,
+                 level,
+                 max_hp,
+                 attack,
+                 defense,
+                 speed,
+                 special):
+        self.pokemon_id = pokemon_id
+        self.current_hp = current_hp
+        self.status = status
+        self.type_1 = type_1
+        self.type_2 = type_2
+        self.catch_rate = catch_rate
+        self.move_1 = move_1
+        self.move_2 = move_2
+        self.move_3 = move_3
+        self.move_4 = move_4
+        self.trainer_id = trainer_id
+        self.experience = experience
+        self.hp_ev = hp_ev
+        self.attack_ev = attack_ev
+        self.defense_ev = defense_ev
+        self.speed_ev = speed_ev
+        self.special_ev = special_ev
+        self.attack_defense_iv = attack_defense_iv
+        self.speed_special_iv = speed_special_iv
+        self.pp_move_1 = pp_move_1
+        self.pp_move_2 = pp_move_2
+        self.pp_move_3 = pp_move_3
+        self.pp_move_4 = pp_move_4
+        self.level = level
+        self.max_hp = max_hp
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.special = special
+        
 class Gen1MemoryManager:
 
     def __init__(self, pyboy, byte_order='big'):
         self.pyboy = pyboy
         self.byte_order = byte_order
 
-    def get_pokemon_1_info(self):
-        return self.pyboy.get_memory_value(POKEMON_1_BASE + LEVEL[0])
+    def _load_pokemon_from_address(self, pokemon_base_address):
+
+        pokemon_values = []
+
+        for offset_enum in PokemonOffsets:
+            offset, num_bytes = offset_enum.value[0], offset_enum.value[1]
+            memory_value = self.pyboy.get_memory_value(pokemon_base_address+offset, num_bytes)
+            pokemon_values.append(memory_value)
+
+        return Pokemon(*pokemon_values)
+    
+    def load_pokemon_from_party(self, party_location):
+        # party_location goes from 1 to 6
+        pokemon_base_address = list(PokemonBaseAddrs)[party_location-1].value
+        return self._load_pokemon_from_address(pokemon_base_address)
