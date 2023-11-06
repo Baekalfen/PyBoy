c1ccbc4a2547b3535ca3e264a0ac80ad39838995
Patrick McQuay <patrickm@vrlsec.com>
2023-11-06 15:14:23 -0500

    Add defaults tring to fill in nonexistent move slots in pretty printing

--- pyboy/plugins/game_wrapper_pokemon_gen1/core/move.py
+++ pyboy/plugins/game_wrapper_pokemon_gen1/core/move.py
@@ -1,14 +1,18 @@
-from ..data.moves.constants import MoveIds
+from ..data.constants.moves import MoveIds
 
 class Move():
+
+    NONEXISTENT_MOVE_STR = "---"
 
     def __init__(self, move_id):
         pass
 
     # We can cheat with just using the enum name as the move name
     # since there is no move that has any special characters in it
-    @staticmethod
-    def get_name_from_id(move_id, camel_case=False):
+    @classmethod
+    def get_name_from_id(cls, move_id, camel_case=False):
+        if move_id == 0:
+            return cls.NONEXISTENT_MOVE_STR
         move_name = MoveIds(move_id).name.replace('_', ' ')
         if camel_case:
             return move_name.title()
