7766dfa92fbc1ab17e6e26a26ff651e55fd19115
SnarkAttack <patrickm@vrlsec.com>
2023-11-08 09:44:42 -0500

    rename function calls to indicate these are memory reads

--- pyboy/plugins/game_wrapper_pokemon_gen1/core/mem_manager.py
+++ pyboy/plugins/game_wrapper_pokemon_gen1/core/mem_manager.py
@@ -8,13 +8,13 @@ class MemoryManager():
     def read_byte(self, addr):
         return self.pyboy.get_memory_value(addr)
     
-    def read_address_from_bytes(self, addr, num_bytes):
+    def read_address_from_memory(self, addr, num_bytes):
         p_addr = 0
         for i in range(num_bytes):
             p_addr += self.pyboy.get_memory_value(addr+i) << i*8
         return p_addr
     
-    def read_hex_from_bytes(self, addr, num_bytes):
+    def read_hex_from_memory(self, addr, num_bytes):
         bytes = []
         for i in range(num_bytes):
             bytes.append(self.pyboy.get_memory_value(addr + i))
@@ -22,7 +22,7 @@ class MemoryManager():
         # need to read this in little endian for Pokemon gen 1
         return int.from_bytes(bytes, byteorder='big')
 
-    def read_bcd_from_bytes(self, addr, num_bytes):
+    def read_bcd_from_memory(self, addr, num_bytes):
         byte_str = ""
         for i in range(num_bytes):
             byte_str += "%x"%self.pyboy.get_memory_value(addr)
