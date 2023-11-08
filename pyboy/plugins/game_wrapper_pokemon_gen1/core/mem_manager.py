bb0f6268c979ba95daa6dc386232db553de166ed
SnarkAttack <patrickm@vrlsec.com>
2023-11-08 09:43:33 -0500

    Define explicit function calls for different type of memory access

--- pyboy/plugins/game_wrapper_pokemon_gen1/core/mem_manager.py
+++ pyboy/plugins/game_wrapper_pokemon_gen1/core/mem_manager.py
@@ -8,7 +8,21 @@ class MemoryManager():
     def read_byte(self, addr):
         return self.pyboy.get_memory_value(addr)
     
-    def read_int_from_bytes(self, addr, num_bytes):
+    def read_address_from_bytes(self, addr, num_bytes):
+        p_addr = 0
+        for i in range(num_bytes):
+            p_addr += self.pyboy.get_memory_value(addr+i) << i*8
+        return p_addr
+    
+    def read_hex_from_bytes(self, addr, num_bytes):
+        bytes = []
+        for i in range(num_bytes):
+            bytes.append(self.pyboy.get_memory_value(addr + i))
+        # Do not believe there is ever a case where we would 
+        # need to read this in little endian for Pokemon gen 1
+        return int.from_bytes(bytes, byteorder='big')
+
+    def read_bcd_from_bytes(self, addr, num_bytes):
         byte_str = ""
         for i in range(num_bytes):
             byte_str += "%x"%self.pyboy.get_memory_value(addr)
