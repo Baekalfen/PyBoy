00036f2e8eec6237b953a0dc24106f0e4ed020ea
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2023-11-22 20:18:23 +0100

    Fix screen.py ordering for docs

--- pyboy/api/screen.py
+++ pyboy/api/screen.py
@@ -31,6 +31,8 @@ class Screen:
     """
     def __init__(self, mb):
         self.mb = mb
+
+        self.raw_buffer = self.mb.lcd.renderer._screenbuffer
         """
         Provides a raw, unfiltered `bytes` object with the data from the screen. Check
         `Screen.raw_buffer_format` to see which dataformat is used. The returned type and dataformat are
@@ -43,7 +45,7 @@ class Screen:
         bytes:
             92160 bytes of screen data in a `bytes` object.
         """
-        self.raw_buffer = self.mb.lcd.renderer._screenbuffer
+        self.raw_buffer_dims = self.mb.lcd.renderer.buffer_dims
         """
         Returns the dimensions of the raw screen buffer.
 
@@ -52,7 +54,7 @@ class Screen:
         tuple:
             A two-tuple of the buffer dimensions. E.g. (160, 144).
         """
-        self.raw_buffer_dims = self.mb.lcd.renderer.buffer_dims
+        self.raw_buffer_format = self.mb.lcd.renderer.color_format
         """
         Returns the color format of the raw screen buffer.
 
@@ -61,7 +63,14 @@ class Screen:
         str:
             Color format of the raw screen buffer. E.g. 'RGB'.
         """
-        self.raw_buffer_format = self.mb.lcd.renderer.color_format
+        if not Image:
+            logger.warning("Cannot generate screen image. Missing dependency \"Pillow\".")
+            self.image = None
+        else:
+            self.image = Image.frombuffer(
+                self.mb.lcd.renderer.color_format, self.mb.lcd.renderer.buffer_dims[::-1],
+                self.mb.lcd.renderer._screenbuffer_raw
+            )
         """
         Generates a PIL Image from the screen buffer.
 
@@ -74,14 +83,8 @@ class Screen:
         PIL.Image:
             RGB image of (160, 144) pixels
         """
-        if not Image:
-            logger.warning("Cannot generate screen image. Missing dependency \"Pillow\".")
-            self.image = None
-        else:
-            self.image = Image.frombuffer(
-                self.mb.lcd.renderer.color_format, self.mb.lcd.renderer.buffer_dims[::-1],
-                self.mb.lcd.renderer._screenbuffer_raw
-            )
+        self.ndarray = np.frombuffer(self.mb.lcd.renderer._screenbuffer_raw, dtype=np.uint8).reshape(ROWS, COLS,
+                                                                                                     4)[:, :, 1:]
         """
         Provides the screen data in NumPy format. The dataformat is always RGB.
 
@@ -90,8 +93,6 @@ class Screen:
         numpy.ndarray:
             Screendata in `ndarray` of bytes with shape (160, 144, 3)
         """
-        self.ndarray = np.frombuffer(self.mb.lcd.renderer._screenbuffer_raw, dtype=np.uint8).reshape(ROWS, COLS,
-                                                                                                     4)[:, :, 1:]
 
     @property
     def tilemap_position_list(self):
