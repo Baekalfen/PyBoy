bc2c544465b06a8b2837b10c2668cf68f9373908
SnarkAttack <patrickm@vrlsec.com>
2023-12-05 12:29:59 -0500

    Fix check on game_state.is_in_battle (was returning opposite of desired)

--- pyboy/plugins/game_wrapper_pokemon_gen1/core/game_state.py
+++ pyboy/plugins/game_wrapper_pokemon_gen1/core/game_state.py
@@ -9,11 +9,15 @@ class GameState():
         self._battle_type = battle_type
 
     def is_in_battle(self):
-        return self._battle_type == 0
+        return self._battle_type != 0
+    
+    @property
+    def battle_type(self):
+        return self._battle_type
     
     @staticmethod
     def load_game_state(mem_manager):
 
-        battle_type = mem_manager.read_hex_from_mem_address(GameStateAddress.BATTLE_TYPE.value)
+        battle_type = mem_manager.read_hex_from_mem_addr(GameStateAddress.BATTLE_TYPE.value)
 
         return GameState(battle_type)
