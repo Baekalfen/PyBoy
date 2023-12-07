49f58d80d2a57d8727ca0c0987c0ef7b4c846be8
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2023-12-07 21:38:20 +0100

    Update bcd test to use "byteorder"

--- tests/test_bcd_conversion.py
+++ tests/test_bcd_conversion.py
@@ -1,29 +1,29 @@
-from pyboy.utils import BCDConverter,EndianType
+from pyboy import bcd_to_dec, dec_to_bcd
+
 
 def test_bcd_to_dec_single_byte():
-    converter = BCDConverter()
-    assert converter.bcd_to_dec(0b00110000) == 30
+    assert bcd_to_dec(0b00110000) == 30
+
 
 def test_dec_to_bcd_single_byte():
-    converter = BCDConverter()
-    assert converter.dec_to_bcd(30) == 0b00110000
+    assert dec_to_bcd(30) == 0b00110000
+
 
 def test_bcd_to_dec_multi_byte():
-    converter = BCDConverter()
-    assert converter.bcd_to_dec(0b0011000000110000, byte_width=2) == 3030
+    assert bcd_to_dec(0b0011000000110000, byte_width=2) == 3030
+
 
 def test_dec_to_bcd_multi_byte():
-    converter = BCDConverter()
-    assert converter.dec_to_bcd(3030, byte_width=2) == 0b0011000000110000
+    assert dec_to_bcd(3030, byte_width=2) == 0b0011000000110000
+
 
 def test_bcd_to_dec_complex():
-    converter = BCDConverter()
-    assert converter.bcd_to_dec(0b011101100101010010001001, byte_width=3) == 765489
+    assert bcd_to_dec(0b011101100101010010001001, byte_width=3) == 765489
+
 
 def test_dec_to_bcd_complex():
-    converter = BCDConverter()
-    assert converter.dec_to_bcd(765489, byte_width=3) == 0b011101100101010010001001
+    assert dec_to_bcd(765489, byte_width=3) == 0b011101100101010010001001
+
 
 def test_bcd_to_dec_complex2():
-    converter = BCDConverter()
-    assert converter.bcd_to_dec(0b10000000000000000000000000000000001000000, byte_width=6, endian_type=EndianType.LITTLE) == 10000000040
+    assert bcd_to_dec(0b10000000000000000000000000000000001000000, byte_width=6, byteorder="little") == 10000000040
