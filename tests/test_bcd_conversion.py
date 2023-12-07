63cb19e79d29e091b145cff65989562848e8651f
Mads Ynddal <5528170+Baekalfen@users.noreply.github.com>
2023-12-07 22:34:57 +0100

    Added partial test for MemoryScanner

--- tests/test_bcd_conversion.py
+++ tests/test_bcd_conversion.py
@@ -1,4 +1,5 @@
-from pyboy import bcd_to_dec, dec_to_bcd
+from pyboy import PyBoy, bcd_to_dec, dec_to_bcd
+from pyboy.api.memoryscanner import DynamicComparisonType
 
 
 def test_bcd_to_dec_single_byte():
@@ -27,3 +28,36 @@ def test_dec_to_bcd_complex():
 
 def test_bcd_to_dec_complex2():
     assert bcd_to_dec(0b10000000000000000000000000000000001000000, byte_width=6, byteorder="little") == 10000000040
+
+
+def test_memoryscanner(kirby_rom):
+    pyboy = PyBoy(kirby_rom, window_type="SDL2", game_wrapper=True)
+    pyboy.set_emulation_speed(0)
+    kirby = pyboy.game_wrapper()
+    kirby.start_game()
+    addresses = pyboy.memory_scanner.scan_memory()
+    assert len(addresses) == 0x10000
+    addresses = pyboy.memory_scanner.scan_memory(start_addr=0xC000, end_addr=0xDFFF)
+    assert len(addresses) == 0xDFFF - 0xC000 + 1
+    addresses = pyboy.memory_scanner.rescan_memory(DynamicComparisonType.INCREASED)
+    assert len(addresses) == 0
+
+    # Find score address
+    pyboy.tick(1, True)
+    pyboy.screen.image.show()
+    score = 0
+    addresses = pyboy.memory_scanner.scan_memory(score, start_addr=0xC000, end_addr=0xDFFF)
+    assert len(addresses) > 1000, "We expected more values to be 0"
+    pyboy.button_press("right")
+
+    pyboy.tick(175, True) # Walk for 280 ticks
+    pyboy.screen.image.show()
+    addresses1 = pyboy.memory_scanner.rescan_memory(DynamicComparisonType.INCREASED)
+    values1 = [pyboy.memory[x] for x in addresses]
+
+    pyboy.tick(125, True) # Walk for 280 ticks
+    addresses2 = pyboy.memory_scanner.rescan_memory(DynamicComparisonType.INCREASED)
+    values2 = [pyboy.memory[x] for x in addresses]
+    pyboy.screen.image.show()
+    breakpoint()
+    pass
