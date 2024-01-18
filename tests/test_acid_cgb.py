e6f45b5feab69021900aacafa41c44adc82ec677
Mads Ynddal <mads@ynddal.dk>
2024-01-18 22:42:42 +0100

    Move screen buffer metadata to attributes buffer, output real RGBA data

--- tests/test_acid_cgb.py
+++ tests/test_acid_cgb.py
@@ -26,8 +26,9 @@ def test_cgb_acid(cgb_acid_file):
         png_path.parents[0].mkdir(parents=True, exist_ok=True)
         image.save(png_path)
     else:
-        old_image = PIL.Image.open(png_path)
-        diff = PIL.ImageChops.difference(image.convert(mode="RGB"), old_image)
+        # Converting to RGB as ImageChops.difference cannot handle Alpha: https://github.com/python-pillow/Pillow/issues/4849
+        old_image = PIL.Image.open(png_path).convert("RGB")
+        diff = PIL.ImageChops.difference(image.convert("RGB"), old_image)
         if diff.getbbox() and not os.environ.get("TEST_CI"):
             image.show()
             old_image.show()
