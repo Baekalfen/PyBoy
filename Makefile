aaf834e71c2b0b62e1f7f8d7a141dd8979bb10cd
Mads Ynddal <mads@ynddal.dk>
2024-12-06 09:44:04 +0100

    Automatically add documentation for plugins to PyBoy constructor

--- Makefile
+++ Makefile
@@ -84,6 +84,7 @@ benchmark:
 docs: clean
 	bash -O extglob -c 'rm -rf -- ${ROOT_DIR}/docs/!(templates|CNAME)'
 	mkdir -p ${ROOT_DIR}/docs/templates
+	cd ${ROOT_DIR}/pyboy/plugins && ${PY} manager_gen.py
 	pdoc --html --force -c latex_math=True -c sort_identifiers=False -c show_type_annotations=True --template-dir docs/templates pyboy
 	cp -r html/pyboy/ ${ROOT_DIR}/docs/
 	rm -rf html
--- pyboy/plugins/manager_gen.py
+++ pyboy/plugins/manager_gen.py
@@ -2,7 +2,16 @@
 # License: See LICENSE.md file
 # GitHub: https://github.com/Baekalfen/PyBoy
 #
+
+import importlib
+import importlib.util
+import os
 import re
+import sys
+
+# Makes us able to import PyBoy from the directory below
+file_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, file_path + "/../..")
 
 # Plugins and priority!
 # E.g. DisableInput first
@@ -178,7 +187,7 @@ if __name__ == "__main__":
 
                 skip_lines(line_iter, "# docs exclude end")
 
-                for p in (set(all_plugins) - set(game_wrappers)) | set(["manager", "manager_gen"]):
+                for p in sorted(list((set(all_plugins) - set(game_wrappers)) | set(["manager", "manager_gen"]))):
                     p_name = to_snake_case(p)
                     lines.append(f'"{p_name}": False,\n')
 
@@ -188,4 +197,56 @@ if __name__ == "__main__":
                 out_lines.append(line)
 
     with open("__init__.py", "w") as f:
+        f.writelines(out_lines)
+
+    out_lines = []
+    with open("../pyboy.py", "r") as f:
+        line_iter = iter(f.readlines())
+        while True:
+            line = next(line_iter, None)
+            if line is None:
+                break
+
+            # Find place to inject
+            if line.strip().startswith("## Plugin kwargs:"):
+                lines = [line.strip() + "\n"]
+                indentation = " " * line.index("## Plugin kwargs:")
+
+                skip_lines(line_iter, "Other keyword arguments may exist")
+
+                for p in sorted(list(set(plugins) - set(game_wrappers))):
+                    p_name = to_snake_case(p)
+                    spec = importlib.util.spec_from_file_location(
+                        p_name, os.path.dirname(os.path.abspath(__file__)) + "/" + p_name + ".py"
+                    )
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
+
+                    for attr in dir(module):
+                        if attr.startswith("_") or attr == "PyBoyPlugin":
+                            continue
+                        _plugin = getattr(module, attr)
+                        if getattr(_plugin, "__bases__", None) and _plugin.__bases__[0].__name__ == "PyBoyPlugin":
+                            for argv in _plugin.argv:
+                                name, argv_desc = argv
+                                name = name.strip("-").replace("-", "_")
+                                _type = argv_desc.get("type", str).__name__
+                                if argv_desc.get("action") == "store_true":
+                                    _type = "bool"
+                                _help = argv_desc.get("help")
+                                if not _help:
+                                    print("Missing documentation", p_name, argv)
+                                    continue
+                                lines.append(f"* {name} ({_type}): {_help} [plugin: {p}]\n")
+                    del spec, module
+
+                lines.append(
+                    "Other keyword arguments may exist for plugins that are not listed here. They can be viewed by running `pyboy --help` in the terminal.\n"
+                )
+                out_lines.extend([indentation + l for l in lines])
+                out_lines.insert(-1, "\n")  # Avoids whitespace
+            else:
+                out_lines.append(line)
+
+    with open("../pyboy.py", "w") as f:
         f.writelines(out_lines)
