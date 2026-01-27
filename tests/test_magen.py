8eab6fcab0044874d116a7c0b35c7a9494e84114
Mads Ynddal <mads@ynddal.dk>
2026-01-27 18:00:10 +0100

    Add new Magen tests

--- tests/test_magen.py
+++ tests/test_magen.py
@@ -7,6 +7,7 @@ import os.path
 from pathlib import Path
 
 import PIL
+import pytest
 
 from pyboy import PyBoy
 
@@ -14,26 +15,43 @@ OVERWRITE_PNGS = False
 
 
 # https://github.com/alloncm/MagenTests
-def test_magen_test(magen_test_file):
-    pyboy = PyBoy(magen_test_file, window="null")
+@pytest.mark.parametrize(
+    "rom, image",
+    [
+        ("bg_oam_priority.gbc", True),  # Screenshot
+        ("hblank_vram_dma.gbc", False),
+        ("key0_lock_after_boot.gbc", False),
+        ("mbc_oob_sram_mbc1.gbc", False),
+        ("mbc_oob_sram_mbc3.gbc", False),
+        ("mbc_oob_sram_mbc5.gbc", False),
+        ("oam_internal_priority.gbc", True),  # Screenshot
+        ("ppu_disabled_state.gbc", False),
+    ],
+)
+def test_magen_test(rom, image, magen_dir):
+    pyboy = PyBoy(magen_dir + "/" + rom, window="null")
     pyboy.set_emulation_speed(0)
     pyboy.tick(59, True)
     pyboy.tick(25, True)
 
-    png_path = Path(f"tests/test_results/{os.path.basename(magen_test_file)}.png")
-    image = pyboy.screen.image
-    if OVERWRITE_PNGS:
-        png_path.parents[0].mkdir(parents=True, exist_ok=True)
-        image.save(png_path)
+    if image:
+        png_path = Path(f"tests/test_results/magen/{os.path.basename(rom)}.png")
+        image = pyboy.screen.image
+        if OVERWRITE_PNGS:
+            png_path.parents[0].mkdir(parents=True, exist_ok=True)
+            image.save(png_path)
+        else:
+            assert png_path.exists(), "Test result doesn't exist"
+            # Converting to RGB as ImageChops.difference cannot handle Alpha: https://github.com/python-pillow/Pillow/issues/4849
+            old_image = PIL.Image.open(png_path).convert("RGB")
+            diff = PIL.ImageChops.difference(image.convert("RGB"), old_image)
+            if diff.getbbox() and os.environ.get("TEST_VERBOSE_IMAGES"):
+                image.show()
+                old_image.show()
+                diff.show()
+            assert not diff.getbbox(), f"Images are different! {magen_test_file}"
     else:
-        assert png_path.exists(), "Test result doesn't exist"
-        # Converting to RGB as ImageChops.difference cannot handle Alpha: https://github.com/python-pillow/Pillow/issues/4849
-        old_image = PIL.Image.open(png_path).convert("RGB")
-        diff = PIL.ImageChops.difference(image.convert("RGB"), old_image)
-        if diff.getbbox() and os.environ.get("TEST_VERBOSE_IMAGES"):
-            image.show()
-            old_image.show()
-            diff.show()
-        assert not diff.getbbox(), f"Images are different! {magen_test_file}"
+        # Check the screen is green
+        assert pyboy.screen.image.getpixel((0, 0)) == (0, 248, 0, 255)
 
     pyboy.stop(save=False)
