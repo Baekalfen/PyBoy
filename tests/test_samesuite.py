fbdf3ef2438c9b3ae9d5396b613363bfc6b0561d
Mads Ynddal <mads@ynddal.dk>
2026-01-28 21:30:52 +0100

    Clean up Samesuite tests

--- tests/test_samesuite.py
+++ tests/test_samesuite.py
@@ -3,9 +3,7 @@
 # GitHub: https://github.com/Baekalfen/PyBoy
 #
 
-import io
 import os.path
-import platform
 from pathlib import Path
 
 import numpy as np
@@ -14,115 +12,91 @@ import pytest
 
 from pyboy import PyBoy
 
-if platform.python_implementation() == "PyPy":
-    timeout = 15
-else:
-    timeout = 5
-
 OVERWRITE_PNGS = False
-
-saved_state = None
 
 
 @pytest.mark.parametrize(
-    "clean, gb_type, rom",
+    "gb_type, rom",
     [
-        (True, "dmg", "interrupt/ei_delay_halt.gb"),
-        (True, "dmg", "apu/div_write_trigger.gb"),
-        (True, "dmg", "apu/div_write_trigger_volume_10.gb"),
-        (True, "dmg", "apu/div_write_trigger_volume.gb"),
-        (True, "dmg", "apu/div_write_trigger_10.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_freq_change_timing-cgbDE.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_delay.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_sweep.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_duty.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_nrx2_speed_change.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_sweep_restart.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_freq_change_timing-cgb0BC.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_align_cpu.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_sweep_restart_2.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_extra_length_clocking-cgb0B.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_restart.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_duty_delay.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_align.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_freq_change_timing-A.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_volume_div.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_volume.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_stop_div.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_restart_nrx2_glitch.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_stop_restart.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_freq_change.gb"),
-        (True, "dmg", "apu/channel_1/channel_1_nrx2_glitch.gb"),
-        (True, "dmg", "apu/div_trigger_volume_10.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_extra_length_clocking-cgbB.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_freq_change_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_stop_div.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_extra_length_clocking-cgb0.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_restart_during_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_and_glitch.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_restart_stop_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_wave_ram_locked_write.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_shift_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_shift_skip_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_wave_ram_sync.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_stop_delay.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_wave_ram_dac_on_rw.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_first_sample.gb"),
-        (True, "dmg", "apu/channel_3/channel_3_restart_delay.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr_restart.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_frequency_alignment.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr_15_7.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_align.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_equivalent_frequencies.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_volume_div.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr15.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr_7_15.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_extra_length_clocking-cgb0B.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_freq_change.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_delay.gb"),
-        (True, "dmg", "apu/channel_4/channel_4_lfsr_restart_fast.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_align.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_duty_delay.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_duty.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_volume.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_stop_restart.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_align_cpu.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_extra_length_clocking-cgb0B.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_restart.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_stop_div.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_freq_change.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_nrx2_glitch.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_delay.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_volume_div.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_nrx2_speed_change.gb"),
-        (True, "dmg", "apu/channel_2/channel_2_restart_nrx2_glitch.gb"),
-        (True, "cgb", "dma/gdma_addr_mask.gb"),
-        (True, "cgb", "dma/hdma_mode0.gb"),
-        (True, "cgb", "dma/hdma_lcd_off.gb"),
-        (True, "cgb", "dma/gbc_dma_cont.gb"),
-        (True, "dmg", "ppu/blocking_bgpi_increase.gb"),
+        ("dmg", "interrupt/ei_delay_halt.gb"),
+        ("dmg", "apu/div_write_trigger.gb"),
+        ("dmg", "apu/div_write_trigger_volume_10.gb"),
+        ("dmg", "apu/div_write_trigger_volume.gb"),
+        ("dmg", "apu/div_write_trigger_10.gb"),
+        ("dmg", "apu/channel_1/channel_1_freq_change_timing-cgbDE.gb"),
+        ("dmg", "apu/channel_1/channel_1_delay.gb"),
+        ("dmg", "apu/channel_1/channel_1_sweep.gb"),
+        ("dmg", "apu/channel_1/channel_1_duty.gb"),
+        ("dmg", "apu/channel_1/channel_1_nrx2_speed_change.gb"),
+        ("dmg", "apu/channel_1/channel_1_sweep_restart.gb"),
+        ("dmg", "apu/channel_1/channel_1_freq_change_timing-cgb0BC.gb"),
+        ("dmg", "apu/channel_1/channel_1_align_cpu.gb"),
+        ("dmg", "apu/channel_1/channel_1_sweep_restart_2.gb"),
+        ("dmg", "apu/channel_1/channel_1_extra_length_clocking-cgb0B.gb"),
+        ("dmg", "apu/channel_1/channel_1_restart.gb"),
+        ("dmg", "apu/channel_1/channel_1_duty_delay.gb"),
+        ("dmg", "apu/channel_1/channel_1_align.gb"),
+        ("dmg", "apu/channel_1/channel_1_freq_change_timing-A.gb"),
+        ("dmg", "apu/channel_1/channel_1_volume_div.gb"),
+        ("dmg", "apu/channel_1/channel_1_volume.gb"),
+        ("dmg", "apu/channel_1/channel_1_stop_div.gb"),
+        ("dmg", "apu/channel_1/channel_1_restart_nrx2_glitch.gb"),
+        ("dmg", "apu/channel_1/channel_1_stop_restart.gb"),
+        ("dmg", "apu/channel_1/channel_1_freq_change.gb"),
+        ("dmg", "apu/channel_1/channel_1_nrx2_glitch.gb"),
+        ("dmg", "apu/div_trigger_volume_10.gb"),
+        ("dmg", "apu/channel_3/channel_3_extra_length_clocking-cgbB.gb"),
+        ("dmg", "apu/channel_3/channel_3_freq_change_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_stop_div.gb"),
+        ("dmg", "apu/channel_3/channel_3_extra_length_clocking-cgb0.gb"),
+        ("dmg", "apu/channel_3/channel_3_restart_during_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_and_glitch.gb"),
+        ("dmg", "apu/channel_3/channel_3_restart_stop_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_wave_ram_locked_write.gb"),
+        ("dmg", "apu/channel_3/channel_3_shift_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_shift_skip_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_wave_ram_sync.gb"),
+        ("dmg", "apu/channel_3/channel_3_stop_delay.gb"),
+        ("dmg", "apu/channel_3/channel_3_wave_ram_dac_on_rw.gb"),
+        ("dmg", "apu/channel_3/channel_3_first_sample.gb"),
+        ("dmg", "apu/channel_3/channel_3_restart_delay.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr_restart.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr.gb"),
+        ("dmg", "apu/channel_4/channel_4_frequency_alignment.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr_15_7.gb"),
+        ("dmg", "apu/channel_4/channel_4_align.gb"),
+        ("dmg", "apu/channel_4/channel_4_equivalent_frequencies.gb"),
+        ("dmg", "apu/channel_4/channel_4_volume_div.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr15.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr_7_15.gb"),
+        ("dmg", "apu/channel_4/channel_4_extra_length_clocking-cgb0B.gb"),
+        ("dmg", "apu/channel_4/channel_4_freq_change.gb"),
+        ("dmg", "apu/channel_4/channel_4_delay.gb"),
+        ("dmg", "apu/channel_4/channel_4_lfsr_restart_fast.gb"),
+        ("dmg", "apu/channel_2/channel_2_align.gb"),
+        ("dmg", "apu/channel_2/channel_2_duty_delay.gb"),
+        ("dmg", "apu/channel_2/channel_2_duty.gb"),
+        ("dmg", "apu/channel_2/channel_2_volume.gb"),
+        ("dmg", "apu/channel_2/channel_2_stop_restart.gb"),
+        ("dmg", "apu/channel_2/channel_2_align_cpu.gb"),
+        ("dmg", "apu/channel_2/channel_2_extra_length_clocking-cgb0B.gb"),
+        ("dmg", "apu/channel_2/channel_2_restart.gb"),
+        ("dmg", "apu/channel_2/channel_2_stop_div.gb"),
+        ("dmg", "apu/channel_2/channel_2_freq_change.gb"),
+        ("dmg", "apu/channel_2/channel_2_nrx2_glitch.gb"),
+        ("dmg", "apu/channel_2/channel_2_delay.gb"),
+        ("dmg", "apu/channel_2/channel_2_volume_div.gb"),
+        ("dmg", "apu/channel_2/channel_2_nrx2_speed_change.gb"),
+        ("dmg", "apu/channel_2/channel_2_restart_nrx2_glitch.gb"),
+        ("cgb", "dma/gdma_addr_mask.gb"),
+        ("cgb", "dma/hdma_mode0.gb"),
+        ("cgb", "dma/hdma_lcd_off.gb"),
+        ("cgb", "dma/gbc_dma_cont.gb"),
+        ("dmg", "ppu/blocking_bgpi_increase.gb"),
     ],
 )
-def test_samesuite(clean, gb_type, rom, samesuite_dir, boot_cgb_rom, boot_rom, default_rom):
-    global saved_state
-
-    if saved_state is None:
-        # HACK: We load any rom and load it until the last frame in the boot rom.
-        # Then we save it, so we won't need to redo it.
-        pyboy = PyBoy(
-            default_rom,
-            window="null",
-            cgb=gb_type == "cgb",
-            bootrom=boot_cgb_rom if gb_type == "cgb" else boot_rom,
-        )
-        pyboy.set_emulation_speed(0)
-        saved_state = io.BytesIO()
-        pyboy.tick(180 if gb_type == "cgb" else 350, True)
-        pyboy.save_state(saved_state)
-        pyboy.stop(save=False)
-
+def test_samesuite(gb_type, rom, samesuite_dir, boot_cgb_rom, boot_rom, default_rom):
     pyboy = PyBoy(
         samesuite_dir + rom,
         window="null",
@@ -130,11 +104,7 @@ def test_samesuite(clean, gb_type, rom, samesuite_dir, boot_cgb_rom, boot_rom, d
         bootrom=boot_cgb_rom if gb_type == "cgb" else boot_rom,
     )
     pyboy.set_emulation_speed(0)
-    saved_state.seek(0)
-    if clean:
-        pyboy.tick(180 if gb_type == "cgb" else 350, True)
-    else:
-        pyboy.load_state(saved_state)
+    pyboy.tick(180 if gb_type == "cgb" else 350, True)
 
     for _ in range(10):
         if np.all(pyboy.screen.ndarray[:, :, :-1] > 240):
